884891297ad1d6d861ee7889016ea422101e34c6
Buran-company <belyymagaleksiy@mail.ru>
2025-05-27 15:17:57 +0300

    let's get this party started

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+/** Replace this with your actual backend URL */
+const BACKEND_URL = process.env.BACKEND_URL!;
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/pokemon',
+        destination: `${BACKEND_URL}/Prod/pokemon`,
+      },
+      {
+        source: '/api/pokemon/:id',
+        destination: `${BACKEND_URL}/Prod/pokemon/:id`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
