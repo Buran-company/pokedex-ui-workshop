e5b202279008d46a5a7a481a9faa3a0026cf78fa
Buran-company <belyymagaleksiy@mail.ru>
2025-05-27 18:41:48 +0300

    pokemon stats v1

--- src/app/pokemon/[pokemon_id]/pokemon.tsx
+++ src/app/pokemon/[pokemon_id]/pokemon.tsx
@@ -1,5 +1,5 @@
 import Pokemon from "@/model/pokemon";
-import { Row, Col, Container, Image } from 'react-bootstrap';
+import { Row, Col, Container, Image, Card, Badge, ListGroup } from 'react-bootstrap';
 
 
 type Props ={
@@ -8,22 +8,52 @@ type Props ={
 
 
 export default function PokemonComponent(props : Props) {
-   const {pokemon} = props;
+    const {pokemon} = props;
 
+    return (
+    <Container className="mt-4">
+        <Row className="justify-content-md-center mb-4">
+            <Col md="auto"><h1>{pokemon.pokemonName}</h1></Col>
+        </Row>
 
-   return (
-       <Container>
-           <Row className="justify-content-md-center">
-               <Col md="auto"><h1>{pokemon.pokemonName}</h1></Col>
-           </Row>
-           <Row>
-               <Col>
-                   <Image src={pokemon.mainImage} thumbnail />
-               </Col>
-               <Col>
-                   Pokémon Properties
-               </Col>
-           </Row>
-       </Container>
+        <Row>
+            <Col md={4}>
+                <Image src={pokemon.mainImage} fluid rounded />
+            </Col>
+
+            <Col md={8}>
+                <Card>
+                    <Card.Header as="h5">Basic Information</Card.Header>
+                    <ListGroup variant="flush">
+                    <ListGroup.Item><strong>Number:</strong> #{pokemon.pokemonNumber}</ListGroup.Item>
+                    <ListGroup.Item>
+                        <strong>Type:</strong>{" "}
+                        {pokemon.pokemonType.map((type, index) => (
+                        <Badge bg="primary" className="me-1" key={index}>{type}</Badge>
+                        ))}
+                    </ListGroup.Item>
+                    <ListGroup.Item><strong>Evolution:</strong> {pokemon.evolution || "N/A"}</ListGroup.Item>
+                    <ListGroup.Item><strong>Devolution:</strong> {pokemon.devolution || "N/A"}</ListGroup.Item>
+                    <ListGroup.Item>
+                        <strong>Evolution Family:</strong>{" "}
+                        {pokemon.evolutionFamily.map((name, i) => (
+                        <Badge bg="secondary" className="me-1" key={i}>{name}</Badge>
+                        ))}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+
+            <Card className="mt-3">
+                <Card.Header as="h5">Stats</Card.Header>
+                    <ListGroup variant="flush">
+                    <ListGroup.Item><strong>HP:</strong> ❤️ {pokemon.healthPoints}</ListGroup.Item>
+                    <ListGroup.Item><strong>Attack:</strong> ⚔️ {pokemon.attack}</ListGroup.Item>
+                    <ListGroup.Item><strong>Defense:</strong> 🛡️ {pokemon.defense}</ListGroup.Item>
+                    <ListGroup.Item><strong>Speed:</strong> 💨 {pokemon.speed}</ListGroup.Item>
+                </ListGroup>
+            </Card>
+            </Col>
+        </Row>
+    </Container>
    );
 }
